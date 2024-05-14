40c1cef4d64f39f6cf01096c43484d29a208f90b
Noah Rischert <noah.rischertnr@gmail.com>
2024-05-14 13:59:47 +0200

    Improved trial logic to an extent that warrants a first test.

--- src/experiment.ts
+++ src/experiment.ts
@@ -25,8 +25,9 @@ import { welcome_screen } from "./instructions/welcome";
 import { consent_screen, notice_screen } from "./instructions/consent";
 import { browser_screen } from "./instructions/browserCheck";
 
-// trials
-import { createNewTrial } from "./trials/trialProcess";
+// Grid logic and stimuli generation
+import { screenWidth, screenHeight, numColumns, numRows, createGrid, resetGrid, calculateCellSize, generateCircles, Stimulus } from "./gridLogic";
+
 
 
 /**
@@ -58,136 +59,37 @@ export async function run({
 
   /************************************** Experiment **************************************/
 
-  var singleSetTrial = {
-    timeline: [
-        {
-            type: psychophysics, // Adding a psychophysics trial
-            stimuli: [
-                {
-                    obj_type: 'circle',
-                    startX: 250,
-                    startY: 250,
-                    radius: 20,
-                    line_color: '#000000',
-                    fill_color: jsPsych.timelineVariable('color'), // Use variable for color
-                }
-              ],
-              choices: "NO_KEYS",
-              trial_duration: 1000,
-              post_trial_gap: 2000
-        },
-        {
-          type: psychophysics, // Adding a psychophysics trial
-          stimuli: [
-              {
-                  obj_type: 'circle',
-                  startX: 250,
-                  startY: 250,
-                  radius: 20,
-                  line_color: '#000000',
-                  fill_color: jsPsych.timelineVariable('color'), // Use variable for color
-              }
-            ],
-            choices: "NO_KEYS",
-            post_trial_gap: 100
-        },
-        {
-          type: psychophysics, // Adding a psychophysics trial
-          stimuli: [
-              {
-                  obj_type: 'circle',
-                  startX: 250,
-                  startY: 250,
-                  radius: 20,
-                  line_color: '#000000',
-                  fill_color: jsPsych.timelineVariable('color'), // Use variable for color
-              }
-            ],
-            choices: "NO_KEYS",
-            post_trial_gap: 1000
-        },
-    ],
+  // Calculate the grid cell size and create the grid
+  const grid = createGrid(numColumns, numRows);
+  const { cellWidth, cellHeight } = calculateCellSize(screenWidth, screenHeight, numColumns, numRows);
+  
+
+  var trial = {
+    type: psychophysics,
+    stimuli: function(): Stimulus[] {
+        return generateCircles(grid, jsPsych.timelineVariable('numCircles'), cellWidth, cellHeight);
+    },
+    choices: "NO_KEYS",
+    background_color: '#008000',
+    trial_duration: 1000,
+    post_trial_gap: 2000,
     timeline_variables: [
-        { face: 'person-1.jpg', name: 'Alex', color: '#FF0000' },
-        { face: 'person-2.jpg', name: 'Beth', color: '#00FF00' },
-        { face: 'person-3.jpg', name: 'Chad', color: '#0000FF' },
-        { face: 'person-4.jpg', name: 'Dave', color: '#FFFF00' }
-    ]
+        { numCircles: 3 },
+        { numCircles: 6 }
+    ],
+    on_finish: function(data) {
+        // Reset grid after each trial
+        resetGrid(grid, numColumns, numRows);
+
+        // Example: return data about the trial, e.g., how many cells were occupied at the end
+        let occupiedCount = grid.filter(cell => cell.occupied).length;
+        return {occupiedCount: occupiedCount}; // Appends this data to the trial's data
+    },
+    sample: {
+        type: 'with-replacement',
+        size: 10
+    }
 };
-
-const trial = {
-  type: psychophysics,
-  stimuli: generateCircles(grid, 10, calculateCellSize(screenWidth, screenHeight, numColumns, numRows).cellWidth, calculateCellSize(screenWidth, screenHeight, numColumns, numRows).cellHeight),
-  choices: "NO_KEYS",
-  background_color: '#008000',
-  trial_duration: 1000,
-  post_trial_gap: 2000,
-  timeline_variables: [],
-  on_finish: function(data) {
-      // Reset grid after each trial
-      resetGrid(grid, numColumns, numRows);
-
-      // Example: return data about the trial, e.g., how many cells were occupied at the end
-      let occupiedCount = grid.filter(cell => cell.occupied).length;
-      return {occupiedCount: occupiedCount}; // Appends this data to the trial's data
-  }
-};
-
-// Populate timeline_variables with dynamic settings for varied trial configurations
-const dynamicStimuliSettings = [
-  { numCircles: 5, color: '#FF0000' },
-  { numCircles: 8, color: '#00FF00' },
-  { numCircles: 10, color: '#0000FF' }
-];
-
-trial.timeline_variables = dynamicStimuliSettings.map(settings => ({
-  stimuli: generateCircles(grid, settings.numCircles, calculateCellSize(screenWidth, screenHeight, numColumns, numRows).cellWidth, calculateCellSize(screenWidth, screenHeight, numColumns, numRows).cellHeight)
-}));
-
-jsPsych.run([trial]);
-
-
-  // // Create an object to hold the experiment trials
-  // const experiment_line:any[] = [];
-  
-  // Generate experiment trials based on the design parameters (foreach loop)
-  // foreach (let itemType of expInfo.DESIGN.itemTypes) {
-  // for (let itemType of expInfo.DESIGN.itemTypes) {
-  //   for (let condition of expInfo.DESIGN.conditions) {
-  //     for (let i = 0; i < expInfo.DESIGN.nTrialsPerCondition; i++) {
-  //       let presentationTime = condition * 100; // Calculate the presentation time based on the condition
-  //       let encodingDuration:number = 0;
-  
-  //       if (!expInfo.DESIGN.DualSet) {
-  //         if (expInfo.DESIGN.OrientationGroup) {
-  //           // Set the encoding duration based on the item type for the orientation group (i can use objects)
-  //           encodingDuration = itemType === "clock" ? expInfo.TIMING.EncodingDurations.long : expInfo.TIMING.EncodingDurations.short;
-  //         } else {
-  //           // Set the encoding duration based on the item type for the non-orientation group
-  //           encodingDuration = itemType === "dot" ? expInfo.TIMING.EncodingDurations.long : expInfo.TIMING.EncodingDurations.short;
-  //         }
-  //       } else {
-  //         // Set the encoding duration to medium for DualSet trials
-  //         encodingDuration = expInfo.TIMING.EncodingDurations.medium;
-  //       }
-  
-  //       // Create a new trial object and add it to the experiment timeline
-  //       let trial_screen = {
-  //         type: psychophysics,
-  //         stimuli: createNewTrial(condition, itemType),
-  //         choices: expInfo.KEYS.CONTINUE,
-  //         response_ends_trial: false, // Change this to false to prevent the trial from ending on response
-  //         trial_duration: presentationTime,
-  //         post_trial_gap: encodingDuration, // Add the encoding duration as the post-trial gap
-  //       }
-
-  //       experiment_line.push(trial_screen);
-  //     }
-  //   }
-  // }
-  
-  // Randomize the order of the trials (commented out)
-  // experimentTrials.timeline = [...experimentTrials.timeline].sort(() => Math.random() - 0.5);
  
   /************************************** Procedure **************************************/
 
@@ -198,7 +100,7 @@ jsPsych.run([trial]);
   timeline.push(consent_screen);
   timeline.push(notice_screen);
   timeline.push(browser_screen);
-  // timeline.push(experiment_line);
+  timeline.push(trial);
 
   // Run the experiment timeline
   await jsPsych.run(timeline);
--- src/gridLogic.ts
+++ src/gridLogic.ts
@@ -5,20 +5,20 @@
  */
 
 import { varSystem, expInfo } from "./settings"; // import the variables from settings
+import { random } from "@coglabuzh/webpsy.js";
 
-// Define a few variables
-const screenWidth = window.screen.width; // Get the screen width
-const screenHeight = window.screen.height; // Get the screen height
-
-const numColumns = 11; // Set the width of the grid
-const numRows = 6; // Set the height of the grid
+// Get the screen width and height, as well as number of rows and columns
+export const screenWidth = window.screen.width;  // Get the screen width
+export const screenHeight = window.screen.height;  // Get the screen height
+export const numColumns = 11;  // Set the width of the grid
+export const numRows = 6;  // Set the height of the grid
 
 const ADJACENCY_LIMIT = 2; // Set the adjacency limit
 const DIAGONAL_ADJACENCY = 1; // Set the diagonal adjacency
 
 
 // Calculate the cell size based on the screen dimensions and grid size
-function calculateCellSize(screenWidth, screenHeight, numColumns, numRows) {
+export function calculateCellSize(screenWidth, screenHeight, numColumns, numRows) {
     const cellWidth = screenWidth / numColumns;
     const cellHeight = screenHeight / numRows;
     return {
@@ -28,14 +28,14 @@ function calculateCellSize(screenWidth, screenHeight, numColumns, numRows) {
 }
 
 // Define a type for the grid cells
-type GridCell = {
+export type GridCell = {
     id: string;
     occupied: boolean;
     x: number;
     y: number;
 };
 
-function createGrid(numColumns: number, numRows: number): GridCell[] {
+export function createGrid(numColumns: number, numRows: number): GridCell[] {
     const grid: GridCell[] = [];
     const middleColumnIndex = Math.floor(numColumns / 2); // Calculate the middle column index
     for (let row = 0; row < numRows; row++) {
@@ -54,7 +54,7 @@ function createGrid(numColumns: number, numRows: number): GridCell[] {
     return grid;
 }
 
-function selectAndOccupyCell(grid: GridCell[]) {
+export function selectAndOccupyCell(grid: GridCell[]) {
     let availableCells = grid.filter(cell => !cell.occupied);
     if (availableCells.length === 0) return null; // No available cells
 
@@ -64,7 +64,7 @@ function selectAndOccupyCell(grid: GridCell[]) {
     return selectedCell;
 }
 
-function markAdjacentCellsAsOccupied(grid: GridCell[], selectedCell: GridCell) {
+export function markAdjacentCellsAsOccupied(grid: GridCell[], selectedCell: GridCell) {
     grid.forEach(cell => {
         // Check horizontal and vertical proximity (extend to two cells)
         if (
@@ -80,4 +80,40 @@ function markAdjacentCellsAsOccupied(grid: GridCell[], selectedCell: GridCell) {
     selectedCell.occupied = true;
 }
 
+export function resetGrid(grid: GridCell[], numColumns: number, numRows: number) {
+    const middleColumnIndex = Math.floor(numColumns / 2);
+    grid.forEach(cell => {
+        const isBorder = cell.x === 0 || cell.x === numColumns - 1 || cell.y === 0 || cell.y === numRows - 1;
+        const isMiddleColumn = cell.x === middleColumnIndex;
+        cell.occupied = isBorder || isMiddleColumn;
+    });
+}
+
+export function randomColor() {
+    // Generate random RGB values
+    const r = random.randint(0, 256); // Random integer between 0 and 255
+    const g = random.randint(0, 256); // Random integer between 0 and 255
+    const b = random.randint(0, 256); // Random integer between 0 and 255
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+export function generateCircles(grid: GridCell[], numCircles: number, cellWidth: number, cellHeight: number) {
+    const stimuli = [];
+
+    for (let i = 0; i < numCircles; i++) {
+        let cell = selectAndOccupyCell(grid);
+        if (cell) {
+            stimuli.push({
+                obj_type: 'circle',                        // Type of the object
+                startX: cell.x * cellWidth + cellWidth / 2, // Center of the cell
+                startY: cell.y * cellHeight + cellHeight / 2, // Center of the cell
+                radius: Math.min(cellWidth, cellHeight) / 4, // Circle radius
+                line_color: randomColor(),                  // Random line color
+                fill_color: randomColor(),                  // Random fill color
+                });
+        }
+    }
+
+    return stimuli;
+}
 
